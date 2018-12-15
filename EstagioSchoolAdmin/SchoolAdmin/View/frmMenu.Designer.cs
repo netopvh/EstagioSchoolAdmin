f09954479ffa7b51c2186fe81de55a4553674eac
MarioGNeto <mario.geroldineto@gmail.com>
2018-12-15 11:59:09 -0200

    Ajustes no design do frmMenu

--- EstagioSchoolAdmin/SchoolAdmin/View/frmMenu.Designer.cs
+++ EstagioSchoolAdmin/SchoolAdmin/View/frmMenu.Designer.cs
@@ -30,35 +30,61 @@
         {
             this.btnSair = new System.Windows.Forms.Button();
             this.btnFuncionarios = new System.Windows.Forms.Button();
+            this.pnlTop = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnSair
             // 
-            this.btnSair.Location = new System.Drawing.Point(702, 403);
+            this.btnSair.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(664, 535);
             this.btnSair.Name = "btnSair";
-            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.Size = new System.Drawing.Size(104, 32);
             this.btnSair.TabIndex = 0;
             this.btnSair.Text = "SAIR";
-            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.UseVisualStyleBackColor = false;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
             // btnFuncionarios
             // 
-            this.btnFuncionarios.Location = new System.Drawing.Point(41, 31);
+            this.btnFuncionarios.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnFuncionarios.FlatAppearance.BorderSize = 0;
+            this.btnFuncionarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFuncionarios.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFuncionarios.ForeColor = System.Drawing.Color.White;
+            this.btnFuncionarios.Image = global::SchoolAdmin.Properties.Resources.funcionarios_icon;
+            this.btnFuncionarios.Location = new System.Drawing.Point(63, 60);
             this.btnFuncionarios.Name = "btnFuncionarios";
-            this.btnFuncionarios.Size = new System.Drawing.Size(75, 23);
+            this.btnFuncionarios.Size = new System.Drawing.Size(137, 168);
             this.btnFuncionarios.TabIndex = 1;
             this.btnFuncionarios.Text = "Funcionários";
-            this.btnFuncionarios.UseVisualStyleBackColor = true;
+            this.btnFuncionarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFuncionarios.UseVisualStyleBackColor = false;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.SteelBlue;
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(800, 10);
+            this.pnlTop.TabIndex = 2;
             // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pnlTop);
             this.Controls.Add(this.btnFuncionarios);
             this.Controls.Add(this.btnSair);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "School Admin - Menu";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMenu_FormClosing);
             this.ResumeLayout(false);
@@ -69,5 +95,6 @@
 
         private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button btnFuncionarios;
+        private System.Windows.Forms.Panel pnlTop;
     }
 }
