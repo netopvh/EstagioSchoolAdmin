aa6a0a42466165511731e578d106fd4ca9d39c4c
MarioGNeto <mario.geroldineto@gmail.com>
2018-12-20 11:50:13 -0200

    Definição do modelo CargoFuncionario

---
+++ EstagioSchoolAdmin/SchoolAdmin/Model/CargoFuncionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.Model
+{
+    class CargoFuncionario
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value > 0 ? value : 0; }
+        }
+
+        private string cargo;
+
+        public string Cargo
+        {
+            get { return cargo; }
+            set { cargo = value; }
+        }
+
+    }
+}
