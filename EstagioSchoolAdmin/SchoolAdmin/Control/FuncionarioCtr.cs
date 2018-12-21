49103ee35a626f0c5862289625b295a5724090e4
MarioGNeto <mario.geroldineto@gmail.com>
2018-12-21 10:49:51 -0200

    Criação do controller FuncionarioCtr

---
+++ EstagioSchoolAdmin/SchoolAdmin/Control/FuncionarioCtr.cs
@@ -0,0 +1,28 @@
+﻿using SchoolAdmin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.Control
+{
+    class FuncionarioCtr
+    {
+        public void Gravar(Funcionario fun)
+        {
+
+        }
+
+        public void Pesquisar(string fun_nome)
+        {
+
+        }
+
+        public void Excluir(int fun_id)
+        {
+
+        }
+
+    }
+}
