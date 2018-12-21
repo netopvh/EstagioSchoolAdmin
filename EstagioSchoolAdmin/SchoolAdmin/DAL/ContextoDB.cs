4296c1610cf1f080254f280d9f981bf45d97ee28
MarioGNeto <mario.geroldineto@gmail.com>
2018-12-21 11:18:14 -0200

    Configuração do ef6

---
+++ EstagioSchoolAdmin/SchoolAdmin/DAL/ContextoDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.DAL
+{
+    class ContextoDB : DbContext
+    {
+        public ContextoDB() : base("name=escola")
+        {
+            try
+            {
+                Configuration.LazyLoadingEnabled = false;
+                Configuration.ProxyCreationEnabled = false;
+                Database.SetInitializer<DbContext>(null);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
