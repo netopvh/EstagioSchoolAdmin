67e809eabdf0091a13636a8840cc53a831a8ddb4
MarioGNeto <mario.geroldineto@gmail.com>
2019-01-04 10:10:33 -0200

    Criação da validação para cpf.

---
+++ EstagioSchoolAdmin/SchoolAdmin.Tests/validators/CPFValidatorTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using SchoolAdmin.Util.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.Tests.validators
+{
+    [TestFixture]
+    class CPFValidatorTests
+    {
+        [Test]
+        public void DeveInstanciarUmCPFValidator()
+        {
+            var sut = new CPFValidator();
+            Assert.IsNotNull(sut);
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void DeveValidarUmCPFVazioOuNulo(string cpf)
+        {
+            var sut = new CPFValidator();
+
+            Assert.IsFalse(sut.Validar(cpf));
+        }
+
+
+        [TestCase("1112223334")]   // cpf.Length < 11
+        [TestCase("111222333444")] // cpf.Length > 11
+        public void DeveValidarCPFTamanhoInvalido(string cpf)
+        {
+            var sut = new CPFValidator();
+
+            Assert.IsFalse(sut.Validar(cpf));
+        }
+
+        [TestCase("11122233344")]
+        [TestCase("111.222.333-44")]
+        [TestCase("000.000.000-00")]
+        [TestCase("111.111.111-11")]
+        [TestCase("222.222.222-22")]
+        [TestCase("333.333.333-33")]
+        [TestCase("444.444.444-44")]
+        [TestCase("555.555.555-55")]
+        [TestCase("666.666.666-66")]
+        [TestCase("777.777.777-77")]
+        [TestCase("888.888.888-88")]
+        [TestCase("999.999.999-99")]
+        [TestCase("ABC.DEF.GHI-JK")]
+        public void DeveValidarCPFInvalido(string cpf)
+        {
+            var sut = new CPFValidator();
+
+            Assert.IsFalse(sut.Validar(cpf));
+        }
+
+        // referência: "https://www.4devs.com.br/gerador_de_cpf"
+        [TestCase("25811556047")]
+        [TestCase("258.115.560-47")]
+        [TestCase("96707170007")]
+        [TestCase("967.071.700-07")]
+        public void DeveValidarCPFValido(string cpf)
+        {
+            var sut = new CPFValidator();
+
+            Assert.IsTrue(sut.Validar(cpf));
+        }
+
+    }
+}
--- EstagioSchoolAdmin/SchoolAdmin/Util/Validators/CPFValidator.cs
+++ EstagioSchoolAdmin/SchoolAdmin/Util/Validators/CPFValidator.cs
@@ -8,22 +8,71 @@ namespace SchoolAdmin.Util.Validators
 {
     public class CPFValidator
     {
-        public static bool Validar(string cpf)
+        public bool Validar(string cpf)
         {
+            StringValidator validatorStr = new StringValidator();
+            if (!validatorStr.Validar(cpf))
+                return false;
+
             cpf = cpf.Replace(".", "").Replace("-", "").Replace(",", "").Replace(" ", "").Trim();
+
             if (cpf.Length != 11)
                 return false;
 
+            if (cpf.Distinct().Count() == 1)
+            {
+                return false;
+            }
+
             try
             {
-               
+                int[] cpf_array;
+                int digitoVerificador1, digitoVerificador2, somatorio1, somatorio2;
+
+                foreach (char c in cpf)
+                {
+                    if (!char.IsNumber(c))
+                        return false;
+                }
+
+                cpf_array = new int[11];
+                for (int i = 0; i < cpf.Length; i++)
+                {
+                    cpf_array[i] = int.Parse(cpf[i].ToString());
+                }
+
+                somatorio1 = somatorio2 = 0;
+                for (int posicao = 0; posicao < 9; posicao++)
+                {
+                    somatorio1 += cpf_array[posicao] * (10 - posicao);
+                    somatorio2 += cpf_array[posicao] * (11 - posicao);
+                }
+
+                digitoVerificador1 = somatorio1 % 11;
+                digitoVerificador1 = digitoVerificador1 < 2 ? 0 : 11 - digitoVerificador1;
+
+                if (cpf_array[9] != digitoVerificador1)
+                {
+                    return false;
+                }
+
+                somatorio2 += digitoVerificador1 * 2;
+                digitoVerificador2 = somatorio2 % 11;
+                digitoVerificador2 = digitoVerificador2 < 2 ? 0 : 11 - digitoVerificador2;
+
+                if(cpf_array[10] != digitoVerificador2)
+                {
+                    return false;
+                }
+
             }
             catch
             {
                 return false;
             }
 
-            return false;
+            return true;
         }
+
     }
 }
