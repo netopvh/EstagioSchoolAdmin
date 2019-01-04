531aa9447d319807f8663cb30e6aa392314bb6fc
MarioGNeto <mario.geroldineto@gmail.com>
2019-01-04 14:41:05 -0200

    Criação da validação para telefone.

---
+++ EstagioSchoolAdmin/SchoolAdmin.Tests/validators/TelefoneValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SchoolAdmin.Util.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.Tests.validators
+{
+    [TestFixture]
+    class TelefoneValidatorTests
+    {
+        [Test]
+        public void DeveInstaciarTelefoneValidator()
+        {
+            var sut = new TelefoneValidator();
+            Assert.IsNotNull(sut);
+        }
+
+        [TestCase("(44) 2630-673")]    // telefone.Length < 10
+        [TestCase("(44) 99703-47741")] // telefone.Length > 11
+        public void DeveValidarTelefoneInvalido(String telefone)
+        {
+            var sut = new TelefoneValidator();
+            Assert.IsFalse(sut.Validar(telefone));
+        }
+
+        [TestCase("(44) 2630-6732")]
+        [TestCase("(44) 99703-4774")]
+        public void DeveValidarTelefoneValido(String telefone)
+        {
+            var sut = new TelefoneValidator();
+            Assert.IsTrue(sut.Validar(telefone));
+        }
+    }
+}
---
+++ EstagioSchoolAdmin/SchoolAdmin/Util/Validators/TelefoneValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.Util.Validators
+{
+    public class TelefoneValidator
+    {
+        public bool Validar(string telefone)
+        {
+            telefone = telefone.Replace("-", "").Replace(" ", "")
+                                    .Replace("(", "").Replace(")", "").Trim();
+
+            StringValidator StrValidator = new StringValidator();
+            if (!StrValidator.Validar(telefone, 11))
+                return false;
+
+            return telefone.Length > 9;
+        }
+    }
+}
